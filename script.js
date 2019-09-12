32dc3ffd4b1d4911898233e8a25926580607654c
Isabele Prieto Himeno <52426897+Prietoisa@users.noreply.github.com>
2019-09-12 11:29:04 -0300

    funcao e if e fors

---
+++ script.js
@@ -0,0 +1,35 @@
+//WHILE
+
+// let i =0
+
+// let filmes = [ "Star Wars", "Vingadores", "Sharkboy e Lavargil", "Shazam", "Pelé"]
+
+// while (i>filmes.length){
+//     console.log(filmes[i]);
+//     i++
+// }
+
+// console.log ("FOR OF")
+// let filmes = [
+//     {titulo: "Invocação do Mal", classificacao: 16},
+//     {titulo: "Sharkboy e Lavargil", classificacao: 0},
+//     {titulo: "Senhor dos Aneis", classificacao: 12},
+//     {titulo: "Ninja Assassino", classificacao: 18},
+//     {titulo: "Cinquenta Tons de Cinza", classificacao: 18},
+// ]
+
+// let idade = Number(prompt("Diga a sua idade"));
+// while(idade  < 0 || isNaN(idade)){
+//     alert("Digite Inválida. Escreva novamente!");
+//     let idade = Number(prompt("Diga a sua idade"));
+// }
+
+// for (let filme of filmes){
+//         if(idade<=filme.classificacao){
+//         console.log(filme.titulo);
+//     }else{
+//         alert(Você não tem idade para esses filmes);
+//     }
+// }
+
+
---
+++ script2.js
@@ -0,0 +1,25 @@
+// let escada = [
+//     {degrau: "#", numero: 1},
+//     {degrau: "##", numero: 2},
+//     {degrau: "###", numero: 3},
+//     {degrau: "####", numero: 4},
+//     {degrau: "#####", numero: 5},
+//     {degrau: "######", numero: 6},
+// ];
+
+// let material = "#";
+// let qtdDegrau = "6";
+
+let i = 0;
+let material = prompt("Digite com que símbolo quer construir a sua escada");
+let qtdDegrau = Number(prompt("Digite o números de degraus que a sua escada vai ter"));
+let escada = "";
+
+whilen (i < qtdDegrau;i++){
+    escada+=material;
+    console.log(escada);
+
+}
+
+
+
